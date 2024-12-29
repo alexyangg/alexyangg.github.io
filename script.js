f4ddd80ab5f7f73b4a3fca6d39b754ec0dc012d5
Alex Yang <alexemail67@gmail.com>
2024-12-29 14:57:44 -0800

    Improve nav bar

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
+  // const menu = document.querySelector(".menu-links");
+  // const icon = document.querySelector(".hamburger-icon");
+  // menu.classList.toggle("open");
+  // icon.classList.toggle("open");
 }
+
+const navIcon = document.querySelector("#nav-icon");
+const navLinks = document.querySelector(".nav-links");
+
+navIcon.onclick = () => {
+  navLinks.classList.toggle("open");
+};
+
+document.addEventListener("click", (event) => {
+  if (!navIcon.contains(event.target) && !navLinks.contains(event.target)) {
+    navLinks.classList.remove("open");
+  }
+});

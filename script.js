3b352f7838ed7ce412092f1d1284f35a28fe06a9
Alex Yang <alexemail67@gmail.com>
2025-01-02 16:36:32 -0800

    Include project videos in website

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-function toggleMenu() {
-  // const menu = document.querySelector(".menu-links");
-  // const icon = document.querySelector(".hamburger-icon");
-  // menu.classList.toggle("open");
-  // icon.classList.toggle("open");
-}
-
 const navIcon = document.querySelector("#nav-icon");
 const navLinks = document.querySelector(".nav-links");
 
